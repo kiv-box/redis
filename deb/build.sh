d8501fca3bcd98d746a4c4a8281a1448f4b60ce5
azraev <jenkins@jenkins.facetz.net>
2017-01-12 13:22:43 +0300

    Update build script

--- deb/build.sh
+++ deb/build.sh
@@ -2,8 +2,17 @@
 
 REDIS_VERSION='3.2.6'
 
-# wget
-# tar vxf
-# cp debian
-# install dh
-# run dh binary
+mkdir -p target/
+wget http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
+tar -xf redis-${REDIS_VERSION}.tar.gz -C target/
+cp -r debian target/redis-${REDIS_VERSION}
+
+apt-get install -y \
+debhelper \
+dh-systemd \
+libjemalloc-dev \
+procps \
+tcl
+
+cd target/redis-${REDIS_VERSION}
+dh binary
